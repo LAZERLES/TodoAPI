95c9b976d22860d44abb4f1fa1b47699a244fc85
LAZERLES <poompepee@hotmail.com>
2025-11-21 06:50:36 +0700

    Refactor database connection setup and add PostgreSQL dependencies

--- Data/DB.js
+++ Data/DB.js
@@ -1,11 +1,31 @@
 const { Sequelize } = require("sequelize");
 const mysql = require("mysql2");
-require('dotenv').config();
+require("dotenv").config();
+
+let sequelize;
 
 // Database connection setup
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT || 'mysql',
-});
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT || "mysql",
+    }
+  );
+}
 
 module.exports = sequelize;
